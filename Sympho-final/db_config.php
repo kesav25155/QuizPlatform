3f6599114fe1ab486c9ef6d9939f103acf0faa28
Kesav Kumar J <140180607+kesav25155@users.noreply.github.com>
2025-08-02 23:26:53 +0530

    Update db_config.php

--- Sympho-final/db_config.php
+++ Sympho-final/db_config.php
@@ -1,9 +1,9 @@
 <?php
-$host = "aws-0-ap-southeast-1.pooler.supabase.com";
-$port = "6543";
-$dbname = "postgres";
-$user = "postgres.yafbexaaxzpxrrrdnzen";
-$password = "Karadi@2025";
+$host = "******";
+$port = "****";
+$dbname = "*****";
+$user = "******";
+$password = "*****";
 
 $conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 
@@ -11,3 +11,4 @@ if (!$conn) {
     die("Error: Unable to connect to database.");
 }
 ?>
+
